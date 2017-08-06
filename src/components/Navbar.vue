367bd95f079cc44f8424f13fa9f5481dc23a7b26
inaban <25hbaam@gmail.com>
2017-08-05 23:44:56 -0500

    navbar de siempre

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,65 +1,30 @@
 <template>
-<nav class="navbar" id="cssNav">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="http://bulma.io">
-      <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-    </a>
-    <div class="navbar-burger burger" v-bind:class ="{'is-active': isActive}" @click="isActive=!isActive" data-target="navMenuExample">
-      <span></span>
-      <span></span>
-      <span></span>
+<nav class="nav has-shadow hero is-dark">
+  <!-- LA PTMR LANCHIPA DEJA DE MODIFICAR EL NAVBAR; SI VAS A HACERLO SUBE UNO FUNCIONAL >:V -->
+  <div class="container">
+    <div class="nav-left">
+      <figure class="image is-48x48" style="margin-right: 8px;">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png">
+        </figure>
     </div>
-  </div>
-
-  <div id="navMenuExample" class="navbar-menu" v-bind:class ="{'is-active': isActive}">
-  <div class="navbar-item has-dropdown is-hoverable">
-       <!--  <a class="navbar-link  is-active" href="/documentation/overview/start/">
-          Docs
-        </a>
-        <div class="navbar-dropdown ">
-          <a class="navbar-item " href="/documentation/overview/start/">
-            Overview
-          </a>
-          <a class="navbar-item " href="http://bulma.io/documentation/modifiers/syntax/">
-            Modifiers
-          </a>
-          <a class="navbar-item " href="http://bulma.io/documentation/grid/columns/">
-            Grid
-          </a>
-        
-        </div>-->
-      </div>
-
-    <div class="navbar-end">
-    <!--  <a class="navbar-item is-hidden-desktop-only" href="https://github.com/jgthms/bulma" target="_blank">
-        Github
-      </a>
-      <a class="navbar-item is-hidden-desktop-only" href="https://twitter.com/jgthms" target="_blank">
-        Twitter
-      </a> -->
+    <span class="nav-toggle">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+    <div class="nav-right nav-menu">
+      <a class="nav-item is-tab is-hidden-tablet is-active">Home</a>
       
-      <div class="navbar-item">
-        <div class="field is-grouped">
-          <p class="control">
-            <a class="tw-button button is-dark"  href="">
-  <span class="icon">
-    <i class="fa fa-twitter"></i>
-  </span>
-  <span>
-    Perfil
-  </span>
-</a>
-          </p>
-          <p class="control">
-            <a class="button is-dark" href="https://github.com/jgthms/bulma/archive/0.5.0.zip">
-              <span class="icon">
-                <i class="fa fa-facebook"></i>
-              </span>
-              <span>Salir</span>
-            </a>
-          </p>
-        </div>
-      </div>
+      <a class="nav-item is-tab is-hidden-tablet" @click="contar()">Features</a>
+      <a class="nav-item is-tab is-hidden-tablet">Pricing</a>
+      <a class="nav-item is-tab is-hidden-tablet">About</a>
+      <a class="nav-item is-tab">
+        <figure class="image is-24x24" style="margin-right: 8px;">
+          <img src="https://image.freepik.com/iconos-gratis/perfil-silueta-usuario_318-40557.jpg">
+        </figure><span class="has-text-white">Perfil</span>
+        
+      </a>
+      <a class="nav-item is-tab" @click="logout()"><span class="has-text-white">Cerrar Sesión</span></a>
     </div>
   </div>
 </nav>
@@ -67,13 +32,7 @@
 
 <script>
 export default {
-
   name: 'Navbar',
- data(){
-  return {
-    isActive:false,
-  }
- },
   methods: {
     logout(){
       this.$http.post('/api/Users/logout?access_token='+ this.$auth.getToken().token).then((res) => {
@@ -81,13 +40,11 @@ export default {
         this.$router.push('/');
       });
     }
-  },
- 
+  }
 };
 </script>
 
 <style lang="css" scoped>
-
 #cssNav{
 background-color: #3c8dbc;
 }
