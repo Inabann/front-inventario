bba2cff207e0fcdeb5eeaa9cfe3a8d02d6631800
inaban <25hbaam@gmail.com>
2017-08-11 16:36:15 -0500

    lista de ventas 1.0

---
+++ src/components/Venta/ListaProductos.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+	<ul>
+		<li v-for="producto in productos">
+			<div class="tags has-addons">
+			  <span class="tag">{{ producto.modelosId }}</span>
+			  <span class="tag is-primary">{{ producto.cantidad }}</span>
+			</div>
+		</li>
+	</ul>
+</div>
+</template>
+
+<script>
+export default {
+	props: ['detalleVentaId'],
+  name: 'ListaProductos',
+
+  data () {
+    return {
+    	productos: []
+    };
+  },
+  methods:{
+  	getProductos(){
+  		this.$http.get('/api/DetalleVenta/'+this.detalleVentaId+'/venta').then(res => this.productos = res.body)
+  	}
+  },
+  created: function(){
+  	this.getProductos()
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/components/Venta/Ventas.vue
+++ src/components/Venta/Ventas.vue
@@ -1,15 +1,50 @@
 <template>
-<pre>
-hola
-	{{ ventas }}
-</pre>
+<div class="container">
+	<h1 class="title is-3 has-text-centered"><span class="has-text-info">Registro de Ventas Realizadas</span></h1>
+	<b-table :data="ventas" :mobile-cards="true" :paginated="true" :per-page="10" >
+    <template scope="props">
+    	<b-table-column field="fecha_ingreso" label="F. Ingreso" sortable>
+        <span class="tag is-success">
+          <!-- {{ new Date(props.row.fecha_ingreso).toLocaleDateString() }} -->
+          {{ props.row.fecha_ingreso | moment("add","1 days","YYYY / MM / DD") }}
+        </span>
+      </b-table-column>
+      <b-table-column field="tipo" label="Tipo"  sortable>
+        {{ props.row.tipo }}
+      </b-table-column>
+      <b-table-column field="clienteId" label="Cliente" sortable>
+        {{ props.row.cliente.nombre }}
+      </b-table-column>
+      <b-table-column field="direccion" label="Destino" sortable>
+        {{ props.row.direccion }}
+      </b-table-column>
+      <b-table-column field="costo_envio" label="C. Envio">
+        {{ props.row.costo_envio }}
+      </b-table-column>
+      <b-table-column field="" label="Productos">
+        <ListaProductos :detalleVentaId="props.row.id"></ListaProductos>
+      </b-table-column>
+      <b-table-column field="total" label="Total" numeric>
+        {{ props.row.total }}
+      </b-table-column>
+      <b-table-column label="Opciones" >
+        <a class="button is-danger is-small" @click="remove(props.row)" >Eliminar</a>
+      </b-table-column>
+    </template>
+    <div slot="empty" class="has-text-centered">
+      Cargando ...
+    </div>
+  </b-table>
+</div>
 </template>
 
 <script>
+import ListaProductos from '@/components/Venta/ListaProductos'
+
 export default {
 
   name: 'Ventas',
-
+  components: { ListaProductos },
   data () {
     return {
     	ventas: []
@@ -17,7 +52,7 @@ export default {
   },
   methods: {
   	getVentas(){
-  		this.$http.get('/api/DetalleVenta').then(res => this.ventas = res.body).catch(err => console.log(err))
+  		this.$http.get('/api/DetalleVenta?filter=%7B%22include%22%3A%22cliente%22%7D').then(res => this.ventas = res.body).catch(err => console.log(err))
   	}
   },
   created: function(){
